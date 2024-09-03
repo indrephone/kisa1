c24bd324d7903758e77a491de3a7e012128307c8
indrephone <indrephone@gmail.com>
2024-09-03 16:10:16 +0300

    pastilizuoti about ir home page'ai

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,9 +1,25 @@
-const About = () => {
-    return ( 
-        <div>
-            <h2>About</h2>
-        </div>
-     );
-}
- 
-export default About;
+import styled from "styled-components";
+
+// Define a styled component without expecting props for the background URL
+const StyledAbout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url('https://cdn.pixabay.com/photo/2018/02/03/15/40/landscape-3127859_1280.jpg'); // Directly set the background image here
+  background-size: cover;
+  background-position: center;
+  color: white;
+  font-size: 24px;
+  text-align: center;
+`;
+
+const about: React.FC = () => {
+    return (
+      <StyledAbout>
+        <h2>About Us</h2>
+      </StyledAbout>
+    );
+};
+
+export default about;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,26 @@
-const Home = () => {
-    return ( 
-        <div>
-            <h2>Home</h2>
-        </div>
-     );
-}
- 
+import styled from "styled-components";
+import React from "react";
+
+// Define a styled component without expecting props for the background URL
+const StyledHome = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url('https://cdn.pixabay.com/photo/2022/02/20/20/09/field-7025238_1280.jpg'); // Directly set the background image here
+  background-size: cover;
+  background-position: center;
+  color: white;
+  font-size: 24px;
+  text-align: center;
+`;
+
+const Home: React.FC = () => {
+    return (
+      <StyledHome>
+        <h2>Welcome to Our Page</h2>
+      </StyledHome>
+    );
+};
+
 export default Home;
